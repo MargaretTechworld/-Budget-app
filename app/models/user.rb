c2ad4b76ee92ede1d803d8ad03bb6810d2538f86
MargaretTechworld <margarettechworld@gmail.com>
2023-11-22 14:43:06 +0000

    Add pages

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
-  has_one_attached :photo
   has_many :categories
   has_many :items, foreign_key: 'author_id'
   validates :name, presence: true
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,26 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-        sessions: 'users/sessions',
-        registrations: 'users/registrations'
-      }
-  root 'splash_screen#index'
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
   resources :categories, only: [:index, :new, :show, :create] do
-    resources :items, only: [:index, :new, :show, :create] 
+    resources :items, only: [:index, :new, :show, :create]
   end
-
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+
+  devise_scope :user do
+    authenticated :user do
+      root to: 'categories#index', as: :authenticated_root
+    end
+    unauthenticated :user do
+      root to: 'splash_screen#index'
+    end
+  end
 
   # Defines the root path route ("/")
   # root "posts#index"
